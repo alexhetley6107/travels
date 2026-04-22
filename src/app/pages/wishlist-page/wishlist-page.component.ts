2ed09fa09efe7f3ea728100deb3394e674718766
alexhetley6107 <hetleyofficial@gmail.com>
2026-04-22 19:43:27 +0300

    feat: wishlist page

--- src/app/pages/wishlist-page/wishlist-page.component.ts
+++ src/app/pages/wishlist-page/wishlist-page.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { WishlistService } from '../../entities/wishlist/wishlist.service';
+import { PlaceCardComponent } from '../../entities/place/components/place-card/place-card.component';
 
 @Component({
   selector: 'app-wishlist-page',
-  imports: [],
+  imports: [PlaceCardComponent],
   templateUrl: './wishlist-page.component.html',
   styleUrl: './wishlist-page.component.scss',
 })
